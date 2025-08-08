b39879c9b236c0bb2f4d8b53d0b092295ea3e958
./Bash <164813896+SeaBass4201759@users.noreply.github.com>
2025-08-08 15:31:27 -0400

    Update and rename nc_dupe.py to BatNet.py

--- BatNet.py
+++ BatNet.py
@@ -3,15 +3,13 @@ import socket
 import shlex 
 import subprocess 
 import sys 
-import textwrap 
 import threading 
 
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return 
-    output = subprocess.check_output(shlex.split(cmd),
-                                     stderr=subprocess.STDOUT)
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
 
 class NetCat:
